fcc97a9878a345f818bf5d63ae5d8dcc60071d3f
alex-heng <45387057+alex-heng@users.noreply.github.com>
2019-04-15 16:44:16 +0800

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -9,6 +9,7 @@ router.post('/', function(req, res, next) {
   // ここではユーザ名の有無判断のみ行っている
   // パスワード認証の場合はユーザ名、パスワードの有無判断に加えて
   // データストア(DBなど)内の存在チェックを合わせて行う
+  console.log('session id:', req.sessionID);
   if(req.body.userName) {
     // セッションへの格納処理
     // ここでは入力されたユーザ名だけだが、処理要件に応じて
